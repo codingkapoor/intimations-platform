8aea7ace79ae84a466b903c5f1e142b107a94fd0
codingkapoor <mailtoshivamk@gmail.com>
2019-08-24 21:00:21 +0530

    ref

--- employee-api/src/main/scala/com/codingkapoor/employee/api/Employee.scala
+++ employee-api/src/main/scala/com/codingkapoor/employee/api/Employee.scala
@@ -4,16 +4,7 @@ import java.time.LocalDate
 
 import play.api.libs.json.{Format, Json}
 
-case class Employee(id: String, name: String, gender: String, doj: LocalDate, pfn: String) {
-  require(
-    id != null && id.trim.nonEmpty
-      && name != null && name.trim.nonEmpty
-      && gender != null && gender.trim.nonEmpty
-      && doj != null
-      && pfn != null && pfn.trim.nonEmpty,
-    "Id, Name, Gender, Date of joining & PF No. are mandatory information."
-  )
-}
+case class Employee(id: String, name: String, gender: String, doj: LocalDate, pfn: String)
 
 object Employee {
   implicit val format: Format[Employee] = Json.format[Employee]
