9be63ce36ade6c3bdbaa724ed4d7aafe46b23c57
codingkapoor <mailtoshivamk@gmail.com>
2019-09-21 11:24:34 +0530

    fsm + logs + ref

--- employee-impl/src/main/scala/com/codingkapoor/employee/core/EmployeeApplication.scala
+++ employee-impl/src/main/scala/com/codingkapoor/employee/core/EmployeeApplication.scala
@@ -6,9 +6,8 @@ import com.codingkapoor.employee.persistence.write.{EmployeePersistenceEntity, E
 import com.codingkapoor.employee.service.EmployeeServiceImpl
 import com.lightbend.lagom.scaladsl.broker.kafka.LagomKafkaComponents
 import com.lightbend.lagom.scaladsl.persistence.cassandra.WriteSideCassandraPersistenceComponents
-import com.lightbend.lagom.scaladsl.persistence.jdbc.ReadSideJdbcPersistenceComponents
+import com.lightbend.lagom.scaladsl.persistence.slick.ReadSideSlickPersistenceComponents
 import com.lightbend.lagom.scaladsl.server.{LagomApplication, LagomApplicationContext}
-import com.lightbend.lagom.scaladsl.persistence.slick.SlickPersistenceComponents
 import com.softwaremill.macwire._
 import play.api.db.HikariCPComponents
 import play.api.libs.ws.ahc.AhcWSComponents
@@ -16,8 +15,7 @@ import play.api.libs.ws.ahc.AhcWSComponents
 abstract class EmployeeApplication(context: LagomApplicationContext)
   extends LagomApplication(context)
     with LagomKafkaComponents
-    with ReadSideJdbcPersistenceComponents
-    with SlickPersistenceComponents
+    with ReadSideSlickPersistenceComponents
     with WriteSideCassandraPersistenceComponents
     with HikariCPComponents
     with AhcWSComponents {
--- employee-impl/src/main/scala/com/codingkapoor/employee/service/EmployeeServiceImpl.scala
+++ employee-impl/src/main/scala/com/codingkapoor/employee/service/EmployeeServiceImpl.scala
@@ -3,43 +3,72 @@ package com.codingkapoor.employee.service
 import akka.{Done, NotUsed}
 import com.codingkapoor.employee.api
 import com.codingkapoor.employee.api.{Employee, EmployeeService}
-import com.codingkapoor.employee.persistence.read.EmployeeRepository
+import com.codingkapoor.employee.persistence.read.{EmployeeEntity, EmployeeRepository}
 import com.codingkapoor.employee.persistence.write._
 import com.lightbend.lagom.scaladsl.api.ServiceCall
 import com.lightbend.lagom.scaladsl.api.broker.Topic
+import com.lightbend.lagom.scaladsl.api.transport.{BadRequest, NotFound}
 import com.lightbend.lagom.scaladsl.broker.TopicProducer
+import com.lightbend.lagom.scaladsl.persistence.PersistentEntity.InvalidCommandException
 import com.lightbend.lagom.scaladsl.persistence.{EventStreamElement, PersistentEntityRegistry}
+import org.slf4j.LoggerFactory
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class EmployeeServiceImpl(persistentEntityRegistry: PersistentEntityRegistry, employeeRepository: EmployeeRepository) extends EmployeeService {
+
+  import EmployeeServiceImpl._
+
+  private val log = LoggerFactory.getLogger(classOf[EmployeeServiceImpl])
+
   private def entityRef(id: String) = persistentEntityRegistry.refFor[EmployeePersistenceEntity](id)
 
   override def addEmployee(): ServiceCall[Employee, Done] = ServiceCall { employee =>
-    entityRef(employee.id).ask(AddEmployee(employee))
+    entityRef(employee.id).ask(AddEmployee(employee)).recover {
+      case e: InvalidCommandException => throw BadRequest(e.getMessage)
+    }
   }
 
   override def updateEmployee(id: String): ServiceCall[Employee, Done] = { employee =>
-    entityRef(employee.id).ask(UpdateEmployee(employee))
+    entityRef(employee.id).ask(UpdateEmployee(employee)).recover {
+      case e: InvalidCommandException => throw BadRequest(e.getMessage)
+    }
   }
 
   override def getEmployees: ServiceCall[NotUsed, Seq[Employee]] = ServiceCall { _ =>
-    employeeRepository.getEmployees
+    employeeRepository.getEmployees.map(_.map(convertEmployeeReadEntityToEmployee))
   }
 
   override def getEmployee(id: String): ServiceCall[NotUsed, Employee] = ServiceCall { _ =>
-    employeeRepository.getEmployee(id)
+    employeeRepository.getEmployee(id).map { e =>
+      if (e.isDefined) convertEmployeeReadEntityToEmployee(e.get)
+      else {
+        val msg = s"No employee found with id = $id."
+        log.error(msg)
+
+        throw NotFound(msg)
+      }
+    }
   }
 
-  override def employeeTopic: Topic[api.EmployeeEvent] = {
+  override def employeeTopic: Topic[api.EmployeeKafkaEvent] = {
     TopicProducer.singleStreamWithOffset { fromOffset =>
       persistentEntityRegistry.eventStream(EmployeeEvent.Tag, fromOffset)
-        .map(event => (convertEvent(event), event.offset))
+        .map(event => (convertPersistentEntityEventToKafkaEvent(event), event.offset))
     }
   }
 
-  private def convertEvent(eventStreamElement: EventStreamElement[EmployeeEvent]): api.EmployeeEvent = {
-    eventStreamElement.event match {
-      case EmployeeAdded(id, name, gender, doj, pfn) => api.EmployeeAdded(id, name, gender, doj, pfn)
-      case EmployeeUpdated(id, name, gender, doj, pfn) => api.EmployeeUpdated(id, name, gender, doj, pfn)
-    }
+}
+
+object EmployeeServiceImpl {
+
+  private def convertPersistentEntityEventToKafkaEvent(eventStreamElement: EventStreamElement[EmployeeEvent]): api.EmployeeKafkaEvent = {
+  eventStreamElement.event match {
+    case EmployeeAdded(id, name, gender, doj, pfn) => api.EmployeeAddedKafkaEvent(id, name, gender, doj, pfn)
+    case EmployeeUpdated(id, name, gender, doj, pfn) => api.EmployeeUpdatedKafkaEvent(id, name, gender, doj, pfn)
+  }
+}
+  private def convertEmployeeReadEntityToEmployee(e: EmployeeEntity): api.Employee = {
+    api.Employee(e.id, e.name, e.gender, e.doj, e.pfn)
   }
 }
