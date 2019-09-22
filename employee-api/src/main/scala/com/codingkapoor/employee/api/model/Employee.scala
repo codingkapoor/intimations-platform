00ffab6171e6c147f38940e1f49796e2d45c7ea5
codingkapoor <mailtoshivamk@gmail.com>
2019-09-22 20:21:25 +0530

    leaves moved to employee

--- employee-api/src/main/scala/com/codingkapoor/employee/api/model/Employee.scala
+++ employee-api/src/main/scala/com/codingkapoor/employee/api/model/Employee.scala
@@ -4,6 +4,12 @@ import java.time.LocalDate
 
 import play.api.libs.json.{Format, Json}
 
+case class Leaves(earned: Int = 0, sick: Int = 0)
+
+object Leaves {
+  implicit val format: Format[Leaves] = Json.using[Json.WithDefaultValues].format[Leaves]
+}
+
 case class Employee(id: String, name: String, gender: String, doj: LocalDate, pfn: String, isActive: Boolean = true, leaves: Leaves = Leaves())
 
 object Employee {
--- employee-api/src/main/scala/com/codingkapoor/employee/api/model/Leaves.scala
+++
@@ -1,9 +0,0 @@
-package com.codingkapoor.employee.api.model
-
-import play.api.libs.json.{Format, Json}
-
-case class Leaves(earned: Int = 0, sick: Int = 0)
-
-object Leaves {
-  implicit val format: Format[Leaves] = Json.using[Json.WithDefaultValues].format[Leaves]
-}
