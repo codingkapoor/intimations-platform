41824ea1cbb9bd0f84d1f0cfe522f8b5d0c2acbb
codingkapoor <mailtoshivamk@gmail.com>
2020-02-23 23:59:44 +0530

    updateemployee validation when state is none

--- employee-impl/src/test/scala/com/codingkapoor/employee/impl/EmployeePersistenceEntitySpec.scala
+++ employee-impl/src/test/scala/com/codingkapoor/employee/impl/EmployeePersistenceEntitySpec.scala
@@ -4,9 +4,10 @@ import java.time.LocalDate
 
 import akka.actor.ActorSystem
 import akka.testkit.TestKit
-import com.codingkapoor.employee.api.models.{ContactInfo, Employee, Leaves, Location, Role}
+import com.codingkapoor.employee.api.models.{ContactInfo, Employee, EmployeeInfo, Leaves, Location, Role}
 import com.codingkapoor.employee.impl.persistence.write.{EmployeePersistenceEntity, EmployeeSerializerRegistry}
-import com.codingkapoor.employee.impl.persistence.write.models.{AddEmployee, EmployeeAdded, EmployeeCommand, EmployeeEvent, EmployeeState}
+import com.codingkapoor.employee.impl.persistence.write.models.{AddEmployee, EmployeeAdded, EmployeeCommand, EmployeeEvent, EmployeeState, UpdateEmployee}
+import com.lightbend.lagom.scaladsl.persistence.PersistentEntity.InvalidCommandException
 import com.lightbend.lagom.scaladsl.playjson.JsonSerializerRegistry
 import com.lightbend.lagom.scaladsl.testkit.PersistentEntityTestDriver
 import org.scalactic.TypeCheckedTripleEquals
@@ -22,7 +23,8 @@ class EmployeePersistenceEntitySpec extends WordSpec with Matchers with BeforeAn
 
   val empId = 128L
   val employee@e = Employee(empId, "John Doe", "M", LocalDate.parse("2018-01-16"), None, "System Engineer", "PFN001",
-    ContactInfo("+91-9887665776", "mail@johndoe.com"), Location(), Leaves(), List(Role.Employee, Role.Admin))
+    ContactInfo("+91-9912345678", "mail@johndoe.com"), Location(), Leaves(), List(Role.Employee, Role.Admin))
+  val employeeInfo@ei = EmployeeInfo(Some("Sr. System Engineer"), Some(ContactInfo("+91-9912345679", "mail1@johndoe.com")), Some(Location("Chennai", "Tamil Nadu")), Some(List(Role.Employee)))
 
   private def withDriver[T](block: PersistentEntityTestDriver[EmployeeCommand[_], EmployeeEvent, Option[EmployeeState]] => T): T = {
     val driver = new PersistentEntityTestDriver(system, new EmployeePersistenceEntity, empId.toString)
@@ -41,6 +43,15 @@ class EmployeePersistenceEntitySpec extends WordSpec with Matchers with BeforeAn
 
       outcome.events should contain only EmployeeAdded(e.id, e.name, e.gender, e.doj, e.dor, e.designation, e.pfn, e.contactInfo, e.location, e.leaves, e.roles)
       outcome.state should ===(Some(EmployeeState(e.id, e.name, e.gender, e.doj, e.dor, e.designation, e.pfn, e.contactInfo, e.location, e.leaves, e.roles, None, Leaves())))
+      outcome.issues should be(Nil)
+    }
+
+    "validate update employee when no employee with given id found" in withDriver { driver =>
+      val outcome = driver.run(UpdateEmployee(empId, employeeInfo))
+
+      outcome.replies.head.getClass should be(classOf[InvalidCommandException])
+      outcome.events.size should ===(0)
+      outcome.issues should be(Nil)
     }
   }
 
