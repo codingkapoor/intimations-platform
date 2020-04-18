e6c95c06b769d02956c511b98eb68d9dc78fe7e6
codingkapoor <mailtoshivamk@gmail.com>
2020-04-19 05:20:18 +0530

    privileged paternity intimation test case

--- employee-impl/src/test/scala/com/codingkapoor/employee/impl/EmployeePersistenceEntitySpec.scala
+++ employee-impl/src/test/scala/com/codingkapoor/employee/impl/EmployeePersistenceEntitySpec.scala
@@ -4,7 +4,7 @@ import java.time.{LocalDate, LocalDateTime}
 
 import akka.actor.ActorSystem
 import akka.testkit.TestKit
-import com.codingkapoor.employee.api.models.PrivilegedIntimationType.Maternity
+import com.codingkapoor.employee.api.models.PrivilegedIntimationType.{Maternity, Paternity, Sabbatical}
 import com.codingkapoor.employee.api.models.{ContactInfo, Employee, EmployeeInfo, Intimation, IntimationReq, Leaves, Location, PrivilegedIntimation, PrivilegedIntimationType, Request, RequestType, Role}
 import com.codingkapoor.employee.impl.persistence.write.EmployeePersistenceEntity.{already5, balanceExtra, between, computeCredits, getNewLeaves, isWeekend}
 import com.codingkapoor.employee.impl.persistence.write.{EmployeePersistenceEntity, EmployeeSerializerRegistry}
@@ -386,7 +386,7 @@ class EmployeePersistenceEntitySpec extends WordSpec with Matchers with BeforeAn
       )
     }
 
-    "release and credit leaves for an employee that has on ongoing privileged intimation that has few dates that are already consumed and has release date as one of the requested dates" in withDriver { driver =>
+    "release and credit leaves for an employee that has on ongoing privileged maternity intimation that has few dates that are already consumed" in withDriver { driver =>
       val endDate@releaseDate = LocalDate.now()
       val startDate = releaseDate.minusDays(2)
 
@@ -412,6 +412,40 @@ class EmployeePersistenceEntitySpec extends WordSpec with Matchers with BeforeAn
       outcomePrivilegedIntimationUpdated :: outcome.events.toList.tail should ===(
         List(
           PrivilegedIntimationUpdated(e.id, Maternity, startDate, endDate, s"$Maternity Leave", newRequests, now),
+          LastLeavesSaved(newState.id, balanced.earned, balanced.currentYearEarned, balanced.sick, balanced.extra),
+          LeavesCredited(newState.id, newLeaves.earned, newLeaves.currentYearEarned, newLeaves.sick, newLeaves.extra),
+          EmployeeUpdated(newState.id, newState.name, newState.gender, newState.doj, newState.dor, newState.designation, newState.pfn, newState.contactInfo, newState.location, newLeaves, newState.roles),
+          EmployeeReleased(newState.id, releaseDate)
+        )
+      )
+    }
+
+    "release and credit leaves for an employee that has on ongoing privileged paternity intimation that has few dates that are already consumed" in withDriver { driver =>
+      val endDate@releaseDate = LocalDate.now()
+      val startDate = releaseDate.minusDays(2)
+
+      val privilegedIntimation = PrivilegedIntimation(Paternity, startDate, releaseDate.plusDays(2))
+      val initialState = state.copy(leaves = Leaves(extra = 5.0), privilegedIntimationOpt = Some(privilegedIntimation))
+
+      driver.initialize(Some(Some(initialState)))
+
+      val outcome = driver.run(ReleaseEmployee(empId))
+
+      val newState = initialState.copy(privilegedIntimationOpt = Some(PrivilegedIntimation(Paternity, startDate, endDate)))
+
+      val (earnedCredits, sickCredits) = computeCredits(newState)
+      val balanced = balanceExtra(newState.leaves.earned + earnedCredits, newState.leaves.currentYearEarned + earnedCredits, newState.leaves.sick + sickCredits, newState.leaves.extra)
+      val newLeaves = Leaves(balanced.earned, balanced.currentYearEarned, balanced.sick, balanced.extra)
+
+      val newRequests = EmployeePersistenceEntity.between(startDate, endDate).filterNot(isWeekend).map(dt => Request(dt, RequestType.Leave, RequestType.Leave)).toSet
+
+      val now = LocalDateTime.now()
+      val piu = outcome.events.toList.head.asInstanceOf[PrivilegedIntimationUpdated]
+      val outcomePrivilegedIntimationUpdated = PrivilegedIntimationUpdated(piu.empId, piu.privilegedIntimationType, piu.start, piu.end, piu.reason, piu.requests, now)
+
+      outcomePrivilegedIntimationUpdated :: outcome.events.toList.tail should ===(
+        List(
+          PrivilegedIntimationUpdated(e.id, Paternity, startDate, endDate, s"$Paternity Leave", newRequests, now),
           LastLeavesSaved(newState.id, balanced.earned, balanced.currentYearEarned, balanced.sick, balanced.extra),
           LeavesCredited(newState.id, newLeaves.earned, newLeaves.currentYearEarned, newLeaves.sick, newLeaves.extra),
           EmployeeUpdated(newState.id, newState.name, newState.gender, newState.doj, newState.dor, newState.designation, newState.pfn, newState.contactInfo, newState.location, newLeaves, newState.roles),
